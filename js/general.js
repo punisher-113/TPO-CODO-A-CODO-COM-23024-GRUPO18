b9ebce880d45f951679f082dff1f8528a40e83af
Pablo Gustavo Valenti <valenti.pablo@gmail.com>
2023-05-22 17:09:07 -0300

    Listo Ahora si

--- js/general.js
+++ js/general.js
@@ -5,7 +5,7 @@
 
 // Asigna el header de la página
 let tempHeader=`
-        <div class="contacto_header">
+        <div class="contacto_header" id="inicio">
             <a href="mailto:info@laboratorio3d.com.ar" title="Enviar email"><i class="fa-regular fa-envelope"></i> info@laboratorio3d.com.ar</a>
             <a href="https://api.whatsapp.com/send?phone=541132600263&text=Hola,%20te%20podemos%20asesorar%20por%20WhatsApp%20a%20gestionar%20tu%20compra%20por%20este%20canal" title="Contactar por WhatsApp" target="_blank"><i class="fa-brands fa-whatsapp"></i> (+54) 9 11 3260 0263</a>
         </div>
@@ -31,22 +31,28 @@ document.querySelector("header").innerHTML=tempHeader;
 
 // Asigna el footer de la página
 let tempFooter=`
-    <div class="info_footer">
-    <p>Suipacha 2972, Turdera, Buenos Aires, Argentina.
-    Lunes a Viernes 9:00hs a 18:00hs</p>
+    <div class="arriba_footer_grande">
+        <a href="#inicio"><i class="fa-solid fa-angles-up fa-3x" title="Ir arriba"></i></i></a>
     </div>
-<div class="iconos_grandes">
-<a href="mailto:grupo18.codoacodo@gmail.com" title="Enviar email"><i class="fa-regular fa-envelope fa-fw fa-3x"></i></a>
-<a href="https://api.whatsapp.com/send?phone=541132600263&text=Hola,%20te%20podemos%20asesorar%20por%20WhatsApp%20a%20gestionar%20tu%20compra%20por%20este%20canal" title="Contactar por WhatsApp" target="_blank"><i class="fa-brands fa-whatsapp fa-fw fa-3x"></i></a>
-<a href="https://www.discord.com" title="Contactar por Discord" target="_blank"><i class="fa-brands fa-discord fa-fw fa-3x" ></i></a> 
-<a href="http://www.instagram.com" title="Nuestro Instagram" target="_blank"><i class="fa-brands fa-instagram fa-fw fa-3x"></i></a> 
-<a href="https://www.facebook.com" title="Nuestro Facebook" target="_blank"><i class="fa-brands fa-facebook fa-fw fa-3x" ></i></a> 
-</div>
-<div class="iconos_chicos">
-<a href="mailto:grupo18.codoacodo@gmail.com" title="Enviar email"><i class="fa-regular fa-envelope fa-fw fa-2x"></i></a>
-<a href="https://api.whatsapp.com/send?phone=541132600263&text=Hola,%20te%20podemos%20asesorar%20por%20WhatsApp%20a%20gestionar%20tu%20compra%20por%20este%20canal" title="Contactar por WhatsApp"><i class="fa-brands fa-whatsapp fa-fw fa-2x"></i></a>
-<a href="https://www.discord.com" title="Contactar por Disord" target="_blank"><i class="fa-brands fa-discord fa-fw fa-2x"></i></a> 
-<a href="https://www.instagram.com" title="Nuestro Instagram" target="_blank"><i class="fa-brands fa-instagram fa-fw fa-2x"></i></a> 
-<a href="https://www.facebook.com" title="Nuestro Facebook" target="_blank"><i class="fa-brands fa-facebook fa-fw fa-2x"></i></a> 
-</div>`
+    <div class="arriba_footer_chico">
+        <a href="#inicio"><i class="fa-solid fa-angles-up fa-2x"></i></i></a>
+    </div>
+    <div class="info_footer">
+        <p>Suipacha 2972, Turdera, Buenos Aires, Argentina.
+           Lunes a Viernes 9:00hs a 18:00hs</p>
+    </div>
+    <div class="iconos_grandes">
+        <a href="mailto:grupo18.codoacodo@gmail.com" title="Enviar email"><i class="fa-regular fa-envelope fa-fw fa-3x"></i></a>
+        <a href="https://api.whatsapp.com/send?phone=541132600263&text=Hola,%20te%20podemos%20asesorar%20por%20WhatsApp%20a%20gestionar%20tu%20compra%20por%20este%20canal" title="Contactar por WhatsApp" target="_blank"><i class="fa-brands fa-whatsapp fa-fw fa-3x"></i></a>
+        <a href="https://www.discord.com" title="Contactar por Discord" target="_blank"><i class="fa-brands fa-discord fa-fw fa-3x" ></i></a> 
+        <a href="http://www.instagram.com" title="Nuestro Instagram" target="_blank"><i class="fa-brands fa-instagram fa-fw fa-3x"></i></a> 
+        <a href="https://www.facebook.com" title="Nuestro Facebook" target="_blank"><i class="fa-brands fa-facebook fa-fw fa-3x" ></i></a> 
+    </div>
+    <div class="iconos_chicos">
+        <a href="mailto:grupo18.codoacodo@gmail.com" title="Enviar email"><i class="fa-regular fa-envelope fa-fw fa-2x"></i></a>
+        <a href="https://api.whatsapp.com/send?phone=541132600263&text=Hola,%20te%20podemos%20asesorar%20por%20WhatsApp%20a%20gestionar%20tu%20compra%20por%20este%20canal" title="Contactar por WhatsApp"><i class="fa-brands fa-whatsapp fa-fw fa-2x"></i></a>
+        <a href="https://www.discord.com" title="Contactar por Disord" target="_blank"><i class="fa-brands fa-discord fa-fw fa-2x"></i></a> 
+        <a href="https://www.instagram.com" title="Nuestro Instagram" target="_blank"><i class="fa-brands fa-instagram fa-fw fa-2x"></i></a> 
+        <a href="https://www.facebook.com" title="Nuestro Facebook" target="_blank"><i class="fa-brands fa-facebook fa-fw fa-2x"></i></a> 
+    </div>`
 document.querySelector("footer").innerHTML=tempFooter
